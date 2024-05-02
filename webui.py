235e9f1083f4af01050e13ee4f852162cd6c92c4
Rafael Leon <rafael.leon@ericsson.com>
2024-05-02 13:19:16 +0200

    Add colab auto

--- webui.py
+++ webui.py
@@ -677,6 +677,42 @@ def dump_default_english_config():
 
 # dump_default_english_config()
 
+#RAFA Gradio Fix
+import subprocess
+import threading
+import time
+import socket
+
+def iframe_thread(port):
+while True:
+time.sleep(0.5)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('127.0.0.1', port))
+if result == 0:
+break
+sock.close()
+print("\nFooocus finished loading, trying to launch cloudflared (if it gets stuck here cloudflared is having issues)\n")
+p = subprocess.Popen(["cloudflared", "tunnel", "--url", "http://127.0.0.1:{}".format(port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+for line in p.stderr:
+l = line.decode()
+if "trycloudflare.com" in l:
+print("This is the URL to access Fooocus:", l[l.find("https"):], end='')
+
+port = 7865 # Replace with the port number used by Fooocus
+threading.Thread(target=iframe_thread, daemon=True, args=(port,)).start()
+
+also change the code inside of shared.gradio_root.launch(
+TO
+shared.gradio_root.launch(
+inbrowser=args_manager.args.in_browser,
+server_name=args_manager.args.listen,
+server_port=args_manager.args.port,
+allowed_paths=[modules.config.path_outputs],
+blocked_paths=[constants.AUTH_FILENAME]
+)
+#RAFA Gradio Fix
+
+
 shared.gradio_root.launch(
     inbrowser=args_manager.args.in_browser,
     server_name=args_manager.args.listen,
